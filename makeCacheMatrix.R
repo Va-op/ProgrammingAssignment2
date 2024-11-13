43e60035a04b67d88a31fb9aba53b6d3e21798b3
Va-op <vgera_be22@thapar.edu>
2024-11-13 15:11:07 +0530

    Add files via upload

---
+++ makeCacheMatrix.R
@@ -0,0 +1,28 @@
+# Function to create a special matrix object with caching
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+# Function to compute or retrieve the cached inverse
+cacheSolve <- function(x, ...) {
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached inverse")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
+}
+
